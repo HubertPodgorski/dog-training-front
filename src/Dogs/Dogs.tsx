a24c65d91861365c39e1d60d429513e7ddb120d3
Hubert Podgórski <hubert.podgorski@alpaka.in>
2020-12-23 17:51:00 +0100

    WIP

---
+++ src/Dogs/Dogs.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dogs = () => {
+    return <div>Calendar</div>;
+};
+
+export default Dogs;
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,24 +1,24 @@
-import {
-    BrowserRouter as ReactRouter,
-    Switch,
-    Route
-} from "react-router-dom";
-import MainList from "./MainList/MainList";
-import Configurator from "./Configurator/Configurator";
-import React from "react";
-import Calendar from "./Calendar/Calendar";
+import { BrowserRouter as ReactRouter, Switch, Route } from 'react-router-dom';
+import MainList from './MainList/MainList';
+import Configurator from './Configurator/Configurator';
+import React from 'react';
+import Calendar from './Calendar/Calendar';
 
 export const routePaths = {
-    'list': '/list',
-    'configurator': '/configurator',
-    'calendar': '/calendar',
-}
+    list: '/list',
+    configurator: '/configurator',
+    calendar: '/calendar',
+    dogs: '/dogs',
+    dogTasks: '/dogTasks',
+    peopleTasks: '/peopleTasks',
+    people: '/people',
+};
 
-const Router  = () => (
+const Router = () => (
     <ReactRouter>
         <Switch>
             <Route path={routePaths.list}>
-                <MainList/>
+                <MainList />
             </Route>
             <Route path={routePaths.configurator}>
                 <Configurator />
@@ -26,11 +26,23 @@ const Router  = () => (
             <Route path={routePaths.calendar}>
                 <Calendar />
             </Route>
+            <Route path={routePaths.dogs}>
+                <Dogs />
+            </Route>
+            <Route path={routePaths.dogTasks}>
+                <DogTasks />
+            </Route>
+            <Route path={routePaths.peopleTasks}>
+                <PeopleTasks />
+            </Route>
+            <Route path={routePaths.people}>
+                <People />
+            </Route>
             <Route path="/">
-                <MainList/>
+                <MainList />
             </Route>
         </Switch>
     </ReactRouter>
-)
+);
 
-export default Router
+export default Router;
