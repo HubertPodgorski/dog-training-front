9228eb2cbf9b4a118aa18dbc110d5e92d23f6570
Hubert Podgórski <hubert.podgorski@alpaka.in>
2020-12-23 17:51:19 +0100

    build fix

--- src/helpers/apiRoutes.ts
+++ src/helpers/apiRoutes.ts
@@ -1,6 +1,6 @@
-const devPrefix = 'http://localhost:3001/api';
+// const devPrefix = 'http://localhost:3001/api';
 const prodPrefix = process.env.REACT_APP_API_PREFIX;
-const appendApiPrefix = (suffix: string): string => `${devPrefix}${suffix}`;
+const appendApiPrefix = (suffix: string): string => `${prodPrefix}${suffix}`;
 
 export const apiRoutes = {
     GET: {
