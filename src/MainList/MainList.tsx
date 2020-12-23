aae712d5a210c607efc2702edca3dcdabb2b8b36
Hubert Podgórski <hubert.podgorski@alpaka.in>
2020-12-23 21:39:12 +0100

    fix build

--- src/MainList/MainList.tsx
+++ src/MainList/MainList.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './MainList.module.scss';
 import TasksGrid from '../components/TwoColumnsGrid/TasksGrid';
+import ButtonBar from '../components/ButtonBar/ButtonBar';
 
 const MainList = () => {
     return (
         <>
             <ButtonBar variant="listing" />
+
             <div className={styles.wrapper}>
                 <TasksGrid />
             </div>
