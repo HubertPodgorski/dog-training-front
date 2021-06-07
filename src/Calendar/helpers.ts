3c54729071cea5bee6a141927ef8f86e3fef0890
Hubert Podgórski <hubert.podgorski@alpaka.in>
2021-06-07 19:55:39 +0200

    quick fix after deleting people and dogs

--- src/Calendar/helpers.ts
+++ src/Calendar/helpers.ts
@@ -4,9 +4,11 @@ export const getDogStatus = (
   dogId: string,
   eventDogs: { dog: { id: string }; status: DogEventStatus }[],
 ): DogEventStatus => {
-  const dogFound = eventDogs.find(
-    (dogWithStatus) => !!dogWithStatus && dogWithStatus.dog.id === dogId,
-  )
+  const dogFound =
+    eventDogs &&
+    eventDogs.find(
+      (dogWithStatus) => !!dogWithStatus && !!dogWithStatus.dog && dogWithStatus.dog.id === dogId,
+    )
 
   return dogFound ? dogFound.status : 'untouched'
 }
